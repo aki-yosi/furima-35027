4409d6e094d3cd73c725d77d660526ed3b919401
aki-yosi <80374501+aki-yosi@users.noreply.github.com>
2021-04-16 14:11:00 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to:"products#index"
-  resources :products , only: [:new, :create, :index, :show, :update, :destroy, :edit, :update]
+  resources :products , only: [:new, :create, :index, :show]
 end
