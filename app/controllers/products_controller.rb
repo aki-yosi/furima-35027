7deea73f29b189b89401b8e7b4f67eb3a58629b1
aki-yosi <80374501+aki-yosi@users.noreply.github.com>
2021-04-16 12:42:40 +0900

    修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
     end
 
     def edit
-        @product = Product.find(params[:id])
+        #@product = Product.find(params[:id])
     end
  
     def create
