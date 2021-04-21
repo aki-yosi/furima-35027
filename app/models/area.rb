1f988538a6e6af5d489a8e518f6c0a3b45a4d693
aki-yosi <80374501+aki-yosi@users.noreply.github.com>
2021-04-21 18:42:37 +0900

    修正

--- app/models/area.rb
+++ app/models/area.rb
@@ -21,5 +21,5 @@ class Area < ActiveHash::Base
 
     include ActiveHash::Associations
     has_many :products
-    has_many :destination
+    has_many :destinations
 end
--- app/models/destination_purchase.rb
+++ app/models/destination_purchase.rb
@@ -14,8 +14,8 @@ class DestinationPurchase
     validates :area_id, numericality: {other_than: 0, message: "can't be blank"}
 
     def save
-      purchase = Purchase.create!(product_id: product_id, user_id: user_id)
+      purchase = Purchase.create(product_id: product_id, user_id: user_id)
       # 住所を保存する
-      destination = Destination.create!(phone_number: phone_number, post_code: post_code, area_id: area_id, city: city, address: address, building_name: building_name, purchase_id: purchase.id)
+      destination = Destination.create(phone_number: phone_number, post_code: post_code, area_id: area_id, city: city, address: address, building_name: building_name, purchase_id: purchase.id)
     end
 end
