4625179268abcf50fc94d297ea013f94eac50025
aki-yosi <80374501+aki-yosi@users.noreply.github.com>
2021-04-21 15:29:13 +0900

    修正

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,14 +1,17 @@
 class DestinationsController < ApplicationController
+    before_action :set_product, only: :index
+    before_action :authenticate_user!, only: :index
+    before_action :move_to_index, only: :index
+    before_action :set_destination, only: :index
+    
 
 
     def index
         @destination = DestinationPurchase.new
-        @product = Product.find(params[:product_id])
     end
 
     def create
         @destination = DestinationPurchase.new(destination_params)
-        @product = Product.find(params[:product_id])
         if @destination.valid?
             pay_product
           @destination.save
@@ -16,20 +19,36 @@ class DestinationsController < ApplicationController
         else
           render 'index'
         end
-      end
+    end
     
       private
     
-      def destination_params
+    def destination_params
         params.require(:destination_purchase).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, token: params[:token], product_id: params[:product_id])
-      end
+    end
 
-      def pay_product
+    def pay_product
         Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述
         Payjp::Charge.create(
           amount: @product.price,  # 商品の値段
           card: destination_params[:token],    # カードトークン
           currency: 'jpy'                 # 通貨の種類（日本円）
         )
-      end
+    end
+
+    def move_to_index
+        if current_user.id == @product.user_id
+          redirect_to root_path
+        end
+    end
+
+    def set_product
+        @product = Product.find(params[:product_id])
+    end
+
+    def set_destination
+        if @product.purchase.present? 
+            redirect_to root_path
+        end
+    end
 end
